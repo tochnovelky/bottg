672bb8a27bbb448a1837d5c91a08a9e5791c27d8
tochnovelky <142398058+tochnovelky@users.noreply.github.com>
2023-08-30 17:46:25 +0300

    Меню новая версия

--- main.py
+++ main.py
@@ -1,160 +1,84 @@
-import logging
 import asyncio
+import logging
+import random
+
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher.filters import CommandStart
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.types import ParseMode, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ParseMode, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-# создание объекта бота
-bot = Bot(token='6279505625:AAF8u3JsaNXAuMtzqLmw2taUllw7zv1N2s4')
+logging.basicConfig(level=logging.INFO)
 
-# создание диспетчера
-dp = Dispatcher(bot, storage=MemoryStorage())
+bot = Bot(token='6584652808:AAGdp1-TcXf9RkHiiF9Fuji9w3c46do67Vo')
+dp = Dispatcher(bot)
 
+# Главное меню
+main_keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False, row_width=2)
+button1 = KeyboardButton('Мои командировки')
+button2 = KeyboardButton('Добавить расходы')
+button3 = KeyboardButton('Новая командировка')
+button4 = KeyboardButton('Завершить командировку')
+button5 = KeyboardButton('Как пользоваться')
+main_keyboard.add(button1, button2, button3, button4, button5)
 
-# создание класса-состояния для хранения состояния при обработке команды "Новая командировка"
-class NewTravel(StatesGroup):
-    waiting_for_city = State()
-    waiting_for_date = State()
+# Клавиатура для выбора статьи расходов
+expenses_keyboard = InlineKeyboardMarkup()
+expenses_keyboard.add(InlineKeyboardButton(text="Билеты", callback_data="tickets"),
+                      InlineKeyboardButton(text="Проживание", callback_data="accommodation"),
+                      InlineKeyboardButton(text="Представительские расходы", callback_data="entertainment"),
+                      InlineKeyboardButton(text="Назад", callback_data="main_menu"))
 
+# Клавиатура для списка командировок
+def generate_business_trips():
+    # генерируем список командировок
+    trips = ['Командировка {}'.format(i) for i in range(1, 6)]
+    random.shuffle(trips)
+    return trips
 
-# обработчик команды /start
-@dp.message_handler(commands='start')
-async def process_start_command(message: types.Message):
-    keyboard = ReplyKeyboardMarkup(
-        resize_keyboard=True,
-        one_time_keyboard=True
-    )
-    buttons = [
-        KeyboardButton(text="Мои командировки"),
-        KeyboardButton(text="Новая командировка"),
-        KeyboardButton(text="Завершить командировку"),
-        KeyboardButton(text="Добавить расходы"),
-        KeyboardButton(text="Как пользоваться")
-    ]
-    keyboard.add(*buttons)
-    await message.answer("Выберите действие:", reply_markup=keyboard)
+def construct_business_trips_keyboard(trips):
+    keyboard = InlineKeyboardMarkup()
+    for trip in trips:
+        keyboard.add(InlineKeyboardButton(text=trip, callback_data=trip),)
+    keyboard.add(InlineKeyboardButton(text="Назад", callback_data="main_menu"))
+    return keyboard
 
+@dp.message_handler(commands=['start'])
+async def start_message_handler(message: types.Message):
+    await bot.send_message(message.chat.id, "Выберите действие из предложенного списка:", reply_markup=main_keyboard)
 
-# добавлено: обработчик кнопки "Добавить расходы"
-@dp.message_handler(text=['Добавить расходы'])
-async def process_add_expense(message: types.Message):
-    keyboard = ReplyKeyboardMarkup(
-        resize_keyboard=True,
-        one_time_keyboard=True
-    )
-    buttons = [
-        KeyboardButton(text="Билеты"),
-        KeyboardButton(text="Проживание"),
-        KeyboardButton(text="Представительские расходы"),
-        KeyboardButton(text="Назад")
-    ]
-    keyboard.add(*buttons)
-    await message.answer("Выберите тип расходов:", reply_markup=keyboard)
+@dp.message_handler(lambda message: message.text not in [button1.text, button2.text, button3.text, button4.text, button5.text], content_types=types.ContentType.TEXT)
+async def text_message_handler(message: types.Message):
+    await bot.send_message(message.chat.id, "Неизвестная команда. Пожалуйста, выберите действие из предложенного списка:", reply_markup=main_keyboard)
 
+@dp.message_handler(lambda message: message.text in [button1.text, button2.text, button3.text, button4.text, button5.text], content_types=types.ContentType.TEXT)
+async def menu_buttons_handler(message: types.Message):
+    if message.text == button1.text:
+        trips = generate_business_trips()
+        await bot.send_message(message.chat.id, "Выберите командировку:", reply_markup=construct_business_trips_keyboard(trips))
+    elif message.text == button2.text:
+        await bot.send_message(message.chat.id, "Выберите статью расходов:", reply_markup=expenses_keyboard)
+    elif message.text == button3.text:
+        await bot.send_message(message.chat.id, "Введите город и дату начала командировки:")
+    elif message.text == button4.text:
+        # Здесь должна быть логика завершения командировки
+        trips = generate_business_trips()
+        await bot.send_message(message.chat.id, "Выберите командировку для завершения:", reply_markup=construct_business_trips_keyboard(trips))
+    elif message.text == button5.text:
+        # Здесь должны быть инструкции по использованию
+        await bot.send_message(message.chat.id, "Как пользоваться ботом?", reply_markup=main_keyboard)
 
-# добавлено: обработчик кнопки "Завершить командировку"
-@dp.message_handler(text=['Завершить командировку'])
-async def process_finish_travel(message: types.Message):
-    keyboard = ReplyKeyboardMarkup(
-        resize_keyboard=True,
-        one_time_keyboard=True
-    )
-    buttons = [
-        # вместо примера списка командировок, добавьте свою логику
-        KeyboardButton(text="Командировка 1"),
-        KeyboardButton(text="Командировка 2"),
-        KeyboardButton(text="Назад")
-    ]
-    keyboard.add(*buttons)
-    await message.answer("Выберите командировку для завершения:", reply_markup=keyboard)
+@dp.callback_query_handler(lambda c: c.data == 'main_menu')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await bot.send_message(callback_query.from_user.id, "Выберите действие из предложенного списка:", reply_markup=main_keyboard)
+    await bot.answer_callback_query(callback_query.id)
 
+@dp.callback_query_handler(lambda c: True)
+async def process_callback_button2(callback_query: types.CallbackQuery):
+    trip = callback_query.data
+    await bot.send_message(callback_query.from_user.id, f"Выбрана командировка {trip}")
+    await bot.answer_callback_query(callback_query.id)
 
-# обработчик команды "Новая командировка"
-@dp.message_handler(lambda message: message.text == "Новая командировка", state=None)
-async def new_travel_handler(message: types.Message):
-    await message.answer("Введите название города, в который отправляетесь")
-    await NewTravel.waiting_for_city.set()
+async def main():
+    await dp.start_polling()
 
-
-# обработка ответа на запрос города
-@dp.message_handler(state=NewTravel.waiting_for_city)
-async def process_city(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['city'] = message.text
-    await message.answer("Когда вы отправляетесь в командировку? (введите дату в формате ДД.ММ.ГГГГ)")
-    await NewTravel.next()
-
-
-# обработка ответа на запрос даты
-@dp.message_handler(state=NewTravel.waiting_for_date)
-async def process_date(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['date'] = message.text
-        # вместо простого вывода информации, вы можете добавить логику создания новой командировки с использованием данных из состояния
-        text = f"Вы добавили новую командировку в {data['city']} на {data['date']}"
-    await message.answer(text)
-    await state.finish()
-
-
-# добавлено: обработчик кнопки "Назад"
-@dp.message_handler(text=['Назад'])
-async def process_back(message: types.Message):
-    keyboard = ReplyKeyboardMarkup(
-        resize_keyboard=True,
-        one_time_keyboard=True
-    )
-    buttons = [
-        KeyboardButton(text="Мои командировки"),
-        KeyboardButton(text="Новая командировка"),
-        KeyboardButton(text="Завершить командировку"),
-        KeyboardButton(text="Добавить расходы"),
-        KeyboardButton(text="Как пользоваться")
-    ]
-    keyboard.add(*buttons)
-    await message.answer("Главное меню:", reply_markup=keyboard)
-
-
-# обработчик команды "Мои командировки"
-@dp.message_handler(lambda message: message.text == "Мои командировки")
-async def list_travels(message: types.Message):
-    keyboard = ReplyKeyboardMarkup(
-        resize_keyboard=True,
-        one_time_keyboard=True
-    )
-    buttons = [
-        KeyboardButton(text="Назад")
-    ]
-    keyboard.add(*buttons)
-    # вместо простого вывода информации, вы можете добавить логику отображения списка командировок из вашей базы данных
-    await message.answer("Список ваших командировок:", reply_markup=keyboard)
-
-
-# добавлено: обработчик команды "Как пользоваться"
-@dp.message_handler(lambda message: message.text == "Как пользоваться")
-async def help(message: types.Message):
-    keyboard = ReplyKeyboardMarkup(
-        resize_keyboard=True,
-        one_time_keyboard=True
-    )
-    buttons = [
-        KeyboardButton(text="Назад")
-    ]
-    keyboard.add(*buttons)
-
-    text = """
-    Для создания новой командировки введите "Новая командировка" и следуйте инструкциям.
-    Для просмотра списка своих командировок введите "Мои командировки".
-    Для завершения командировки выберите соответствующий пункт в меню.
-    Для добавления расходов выберите соответствующий пункт в меню.
-    """
-
-    await message.answer(text, reply_markup=keyboard, parse_mode=ParseMode.MARKDOWN)
-
-
-# запуск бота
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    asyncio.run(dp.start_polling())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
